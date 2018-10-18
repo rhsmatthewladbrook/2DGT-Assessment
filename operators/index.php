2341f02c0372ca1ac2eacae787b760e1b949446c
rhsmatthewladbrook <matthewladbrook@riccarton.school.nz>
2018-10-19 11:51:07 +1300

    Created and styled table

---
+++ operators/index.php
@@ -0,0 +1,27 @@
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Nick</td>
+          <td>Type</td>
+          <td>Oceania</td>
+          <td>Name</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Java</td>
+          <td>IRCop</td>
+          <td>Oceania</td>
+          <td>Matthew Ladbrook</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Phoenix</td>
+          <td>ChanOp</td>
+          <td>North America</td>
+          <td>Stuart Rogers</td>
+        </tr>
+      </tbody>
+    </table>
