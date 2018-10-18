fb1dbde9fbe580b45538dab39ce336147f75116d
rhsmatthewladbrook <matthewladbrook@riccarton.school.nz>
2018-10-19 12:04:47 +1300

    Update operator/index.php
    Added the main info layout to start the page.

---
+++ operator/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <h1>Java</h1>
+    <p>ID: 1</p>
+    <p>Operator Type: IRCOp</p>
+    <p>Region: Oceania</p>
+    <p>Name: Matthew Ladbrook</p>
+  </body>
+</html>
